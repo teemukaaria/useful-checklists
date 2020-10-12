0be502106c23789aa3b0ecc3dbef87ad028852e0
Teemu Kääriä <teemu.kaaria@aalto.fi>
2020-10-12 17:44:43 +0300

    refactor: extract common styles and css variables

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,20 +35,13 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: var(--color-text);
+  max-width: 360px;
+  padding: 16px;
+  margin: auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
